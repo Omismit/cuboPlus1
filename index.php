a9d116c489e8ebac909bbb03467821da42f80d94
manuel3g <manuel.3geovanny@gmail.com>
2023-07-10 17:29:54 -0600

    New index and css file
    css file

---
+++ index.php
@@ -0,0 +1,51 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="icon" href="https://icons.veryicon.com/png/o/business/blockchain-commerce/lightning-network.png">
+  <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+  <link rel="stylesheet" href="styles/styles.css">
+</head>
+<body>
+  <div class="container">
+    <div class="row">
+      <div class="col-12 text-center">
+        <h1>Remesas</h1>
+      </div>
+    </div>
+    <div class="row button-container">
+      <div class="col-md-4">
+        <button class="btn btn-success btn-block">1</button>
+      </div>
+      <div class="col-md-4">
+        <button class="btn btn-success btn-block">2</button>
+      </div>
+      <div class="col-md-4">
+        <button class="btn btn-success btn-block">3</button>
+      </div>
+    </div>
+    <div class="row button-container">
+      <div class="col-md-4">
+        <button class="btn btn-success btn-block">4</button>
+      </div>
+      <div class="col-md-4">
+        <button class="btn btn-success btn-block">5</button>
+      </div>
+      <div class="col-md-4">
+        <button class="btn btn-success btn-block">6</button>
+      </div>
+    </div>
+    <div class="row button-container">
+      <div class="col-md-4">
+        <button class="btn btn-success btn-block">7</button>
+      </div>
+      <div class="col-md-4">
+        <button class="btn btn-success btn-block">8</button>
+      </div>
+      <div class="col-md-4">
+        <button class="btn btn-success btn-block">9</button>
+      </div>
+    </div>
+  </div>
+</body>
+</html>
