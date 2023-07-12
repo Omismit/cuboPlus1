de68decae43db406cbedf83f9428b2b8dcad443a
Allan <108568433+RoshPlay@users.noreply.github.com>
2023-07-12 13:30:38 -0600

    Prueba com la api de ZBD

--- index.php
+++ index.php
@@ -5,6 +5,9 @@
   <link rel="icon" href="https://icons.veryicon.com/png/o/business/blockchain-commerce/lightning-network.png">
   <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
   <link rel="stylesheet" href="styles/styles.css">
+  <script src="code.js" defer></script>
+  <script src="qrcode.min.js" defer></script>
+  <script type='module' src='bd.js' defer></script>
 </head>
 <body>
   <div class="container">
@@ -12,6 +15,30 @@
       <div class="col-12 text-center">
         <h1>Remesas</h1>
       </div>
+    
+      <div class="container col-12 ">
+     
+    <div class="col-md-4 text-center">
+    <input type="number" placeholder="typing the amount you wanna insert" id="sats">
+    </div>
+    <div class="col-md-4 text-center">
+    <input type="text" placeholder="Write a description" id="desc">
+    </div>
+    <div class="col-md-4 text-center">
+    <input type="submit" value='enviar' id="enviar">
+    </div>
+      </div>
+
+      <div class="col-md-4 text-center">
+    <input type="button" value='Crear QR' id="createQR">
+    </div>
+
+    <div id="qr"></div>
+<br>
+
+
+
+
     </div>
     <div class="row button-container">
       <div class="col-md-4">
