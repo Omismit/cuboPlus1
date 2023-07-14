9e73f9ba1767f18f8a3e509979b6e205758c9be9
Rodrigo Aguilera <rodrigoaguilerad2000@gmail.com>
2023-07-14 17:33:15 -0600

    payout almost ready

--- js/payout.js
+++ js/payout.js
@@ -1,18 +1,18 @@
 document.addEventListener("DOMContentLoaded", function() {
-    const inputs = document.querySelectorAll("input");
-  
-    inputs.forEach(function(input) {
-      const label = input.nextElementSibling;
-  
-      input.addEventListener("focus", function() {
-        label.classList.add("active");
-      });
-  
-      input.addEventListener("blur", function() {
-        if (this.value === "") {
-          label.classList.remove("active");
-        }
-      });
-    });
+  const expirationInput = document.getElementById("expiration");
+
+  expirationInput.addEventListener("input", function() {
+    const value = this.value.replace(/\D/g, ""); // Eliminar caracteres no numéricos
+    let formattedValue = "";
+
+    if (value.length > 2) {
+      const month = value.slice(0, 2);
+      const year = value.slice(2, 4);
+      formattedValue = `${month}/${year}`;
+    } else {
+      formattedValue = value;
+    }
+
+    this.value = formattedValue.slice(0, 5);
   });
-  
+});
