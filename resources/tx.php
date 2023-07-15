fa5d719e4b44507fc3261b380a01e3c38df5da11
Allan <108568433+RoshPlay@users.noreply.github.com>
2023-07-14 23:52:39 -0600

    tx.php

---
+++ resources/tx.php
@@ -0,0 +1,123 @@
+<?php
+session_start();
+if (!isset($_SESSION['iduser'])) {
+    header('location: ../index.html');
+}
+?>
+<!DOCTYPE html>
+<html lang="es">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Easy Remittance</title>
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css">
+  <link rel="icon" href="https://icons.veryicon.com/png/o/business/blockchain-commerce/lightning-network.png">
+  <link rel="stylesheet" href="../styles/styles.css">
+</head>
+<body>
+  <nav class="navbar navbar-expand-md navbar-light fixed-top custom-navbar">
+    <a class="navbar-brand" href="dashboard.php" style="color:white;"><img src="../img/ln.png" alt="Logo" style="width: 48px;height: 48px;margin-right:10px;">Easy Remittance</a>
+    <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse justify-content-end" id="navbarNav">
+      <ul class="navbar-nav">
+        <li class="nav-item">
+          <span class="navbar-text" style="color:white;padding-right:20px;">
+          <?php echo $_SESSION['user']; ?>
+          </span>
+        </li>
+        <li class="nav-item">
+          <button class="btn btn-primary custom-btn" onclick="location.href = './logout.php';">Cerrar sesión</button>
+        </li>
+      </ul>
+    </div>
+  </nav>
+
+ 
+
+<div class="container mt-5" >
+    <div class="row">
+      <div class="col">
+        <div class="d-flex align-items-center flex-column" style="margin-top:72px;">
+            
+
+
+            <table class="table table-hover" id="tx">
+        <thead>
+          <tr>
+            <th scope="col">#</th>
+            <th scope="col">First</th>
+            <th scope="col">Last</th>
+            <th scope="col">Handle</th>
+            <th scope="col">First</th>
+            <th scope="col">Last</th>
+            <th scope="col">Handle</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <th scope="row">1</th>
+            <td>Mark</td>
+            <td>Otto</td>
+            <td>@mdo</td>
+            <td>Mark</td>
+            <td>Otto</td>
+            <td>@mdo</td>
+          </tr>
+        </tbody>
+        </table>      
+  
+
+                
+       </div>
+      </div>
+    </div>
+</div>
+        <br>
+        <br>
+        <br>
+      </div>
+    </div>
+  </div>
+
+  <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"></script>
+  <script>
+        $(document).ready(function() {
+            getTx();
+        });
+
+        function getTx(){
+            var tr="";
+          $.ajax({
+              method: "POST",
+              url: "store.php",
+              dataType: "json",
+              data: {type: "5"},
+              success: function (data) {
+                  if (data['response']) {
+                      $.each(data["data"],function(index,value){
+                          tr+='<tr><th scope="row">'+value["nameSender"]+'</th><td>'+value["lastnameSender"]+'</td>\n\
+                              <td>'+value["typeTx"]+'</td><td>'+value["nameReceiver"]+'</td><td>'+value["lastnameReceiver"]+'</td>\n\
+                              <td>'+value["fiat"]+'</td><td>'+value["sats"]+'</td></tr>';
+                                  
+                      });
+                      $("#tx > tbody").html(tr);
+                  } else {
+                      $("#alert1").show();
+                  }
+              },
+              error: function () {
+              }
+          });
+        }
+
+    </script>
+
+  
+  
+
+</body>
+</html>
