c8c41d354956d6d1129fcb3687dcc583f9e9e186
manuel3g <manuel.3geovanny@gmail.com>
2023-07-15 01:32:08 -0600

    Update payout

--- resources/payout.php
+++ resources/payout.php
@@ -110,7 +110,7 @@ if (!isset($_SESSION['iduser'])) {
         <script src="https://code.jquery.com/jquery-3.6.0.js"></script>
         <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"></script>
         <script src="https://cdn.jsdelivr.net/npm/jquery-validation@1.19.5/dist/jquery.validate.min.js"></script>
-        <script src="js/payout.js"></script>
+        <script src="../js/payout.js"></script>
         <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js"></script>
     </body>
 </html>
